47480d902bc22d6bf0e408ba691ce9d2b439fbd1
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-11-11 18:49:00 -0300

    Listas Dinâmicas

--- listas.dart
+++ listas.dart
@@ -6,7 +6,7 @@ List<String> nomes = ['Carlos', 'Ana', 'João'];
 // Acesso
 String primeiroNome = nomes[0]; // 'Carlos'
 
-print(primeiroNome);
+//print(primeiroNome);
 
 // Adição
 nomes.add('Maria');
@@ -14,11 +14,27 @@ nomes.add('Maria');
 // Remoção
 nomes.remove('Ana');
 
-print(primeiroNome);
+//print(primeiroNome);
 
 // Iteração
 for (String nome in nomes) {
   print(nome);
-}
+  }
+
+////////////////////////////////////////
+
+// Lista Dinâmica
+
+List<dynamic> listaDinamica = [26, 'Carlim', 'Carlos Aquino', 1.86, true];
+
+print(listaDinamica);
+
+String frase = 'Eu sou ${listaDinamica[1]} \n'
+    'mas meu nome completo é: ${listaDinamica[2]}, \n'
+    'eu me considero geek? ${listaDinamica[4]}. \n'
+    'Eu tenho ${listaDinamica[3]} metros de altura e \n'
+    '${listaDinamica[0]} anos de idade';
+
+print(frase);
 
 }
