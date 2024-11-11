4b44287d73e7a6b46924dc89eb3c1bffbcc48b1e
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-11-11 18:31:24 -0300

    update aula 2

--- projeto_inicial.dart
+++ projeto_inicial.dart
@@ -1,9 +1,15 @@
-void main() {
+void main(){
 
-  int idade = 26;
-  int idadeHex = 0x00001A;
-
-  print(idade);
-  print(idadeHex);
-
+    int idade = 26;
+    double altura = 1.86;
+    bool geek = true;
+    String nome = 'Carlos Aquino';
+    String apelido = 'Carlim';
+    String frase = 'Eu sou $apelido \n'
+        'mas meu nome completo é: $nome, \n'
+        'eu me considero geek? $geek. \n'
+        'Eu tenho $altura metros de altura e \n'
+        '$idade anos de idade';
+        
+    print(frase);
 }
