017e073d6dee4acdc0e81c8d0c79521ca08dd1b2
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-11-14 19:46:40 -0300

    update

--- encapsulamento/pessoa.dart
+++ encapsulamento/pessoa.dart
@@ -8,9 +8,10 @@ class Pessoa {
   // Construtor
   Pessoa(this.nome, this._idade);
 
-  // Getter público para acessar o atributo privado _idade
+  // Getter público para acessar o atributo privado _idade - retorna uma copia com o valor original
   int get getIdade{
-    return _idade;
+    int idade = _idade;
+    return idade;
   }
 
   // Setter público para modificar o atributo privado _idade com uma validação
